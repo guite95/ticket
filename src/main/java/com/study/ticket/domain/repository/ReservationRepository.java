557828081c01d125bfd4a3a5830cd1fe771f35b8
장욱 <janguk95@naver.com>
2025-12-17 22:36:26 +0900

    feat: 조회 메서드 추가

--- src/main/java/com/study/ticket/domain/repository/ReservationRepository.java
+++ src/main/java/com/study/ticket/domain/repository/ReservationRepository.java
@@ -3,5 +3,8 @@ package com.study.ticket.domain.repository;
 import com.study.ticket.domain.Entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    List<Reservation> findByUserId(Long userId);
 }
